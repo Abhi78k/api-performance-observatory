4cba7a08fbe3efaa19bff57b054f7794e6842ea6
itsthatextrovert <sowmyarajarajan.tech@gmail.com>
2026-06-12 15:01:09 +0530

    Database Connectivity Established

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,38 @@
+// package main
+
+// import ("fmt")
+
+// func main() {
+// 	fmt.Println("API Observatory Started")
+// }
+
 package main
 
-import ("fmt")
+import (
+	"fmt"
+
+	"github.com/Abhi78k/api-performance-observatory/internal/database"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	db, err := database.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database connected!")
+
+	_ = db
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "API Observatory",
+		})
+	})
+
+	r.Run(":8080")
+
 }

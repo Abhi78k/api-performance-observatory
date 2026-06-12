8c2fd1fdf4eac6b7d746d554cb3555f002b6eeef
itsthatextrovert <sowmyarajarajan.tech@gmail.com>
2026-06-12 15:35:25 +0530

    Service handler was created and service routes were registered

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Abhi78k/api-performance-observatory/internal/database"
+	"github.com/Abhi78k/api-performance-observatory/internal/handlers"
 	"github.com/Abhi78k/api-performance-observatory/internal/models"
 	"github.com/gin-gonic/gin"
 )
@@ -22,6 +23,16 @@ func main() {
 		&models.User{},
 		&models.Service{},
 	)
+
+	serviceHandler := handlers.ServiceHandler{
+		DB: db,
+	}
+
+	r.POST("/services", serviceHandler.CreateService)
+	r.GET("/services", serviceHandler.GetServices)
+	r.GET("/services/:id", serviceHandler.GetService)
+	r.PUT("/services/:id", serviceHandler.UpdateService)
+	r.DELETE("/services/:id", serviceHandler.DeleteService)
 
 	r := gin.Default()
 
--- internal/handlers/service.go
+++ internal/handlers/service.go
@@ -1,6 +1,42 @@
-package models
+package handlers
 
-type Service struct {
-	ID   uint `gorm:"primaryKey"`
-	Name string
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type ServiceHandler struct {
+	DB *gorm.DB
+}
+
+func (h *ServiceHandler) CreateService(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Create Service - not implemented",
+	})
+}
+
+func (h *ServiceHandler) GetServices(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Get Services - not implemented",
+	})
+}
+
+func (h *ServiceHandler) GetService(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Get Service - not implemented",
+	})
+}
+
+func (h *ServiceHandler) UpdateService(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Update Service - not implemented",
+	})
+}
+
+func (h *ServiceHandler) DeleteService(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Delete Service - not implemented",
+	})
 }

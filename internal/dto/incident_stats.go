38da5ab4e57c3f95a87847631cfd9f9761b3cdd8
SANJANASREE-V <sanjanasree.v.2024.it@rajalakshmi.edu.in>
2026-06-22 15:21:48 +0530

    removed incident stats

--- internal/dto/incident_stats.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type IncidentStatsResponse struct {
-	TotalIncidents         int     `json:"total_incidents"`
-	TotalDowntimeMinutes   float64 `json:"total_downtime_minutes"`
-	AverageIncidentMinutes float64 `json:"average_incident_minutes"`
-	UptimePercentage       float64 `json:"uptime_percentage"`
-}
